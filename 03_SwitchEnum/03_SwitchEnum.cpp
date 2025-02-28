38a16abfcf8c17e528103665b87a188c08549d45
Onoka_nd96@student.itstep.org <Onoka_nd96@student.itstep.org>
2025-02-28 17:56:15 +0000

    LAB 03-04

---
+++ 03_SwitchEnum/03_SwitchEnum.cpp
@@ -0,0 +1,74 @@
+﻿#include<iostream>
+using namespace std;
+
+void main() {
+
+	//      class work assignment from the file 
+
+
+	/*1.Вивести на екран цілі числа від 1 до 100. - while*/
+
+	/*int i = 1;
+	while (i <= 100)
+	{
+		cout << i << endl;
+		i++;
+	}*/
+
+	/*2.Вивести на екран додатні парні числа, 
+	які не перевищують 200, та їх кількість. - while*/
+
+	/*int q = 1;
+	while (q<=200)
+	{
+		if (q%2==0)
+		{
+			cout << q << endl;
+		}
+		q++;
+	}*/
+
+	/*3.Дано N(N > 0).Вводяться N чисел.
+	Знайти суму парних серед цих чисел. - while*/
+
+	/*int countnumber;//10
+	cout << "Enter how many numbers to enter :";  cin >> countnumber;
+	int i = 0;
+	int number;
+	int summa = 0;
+	while (i < countnumber) {
+		cout << "Enter : " << i + 1 << " number : ";
+		cin >> number;
+		if (number % 2 == 0) {
+			summa += number;
+		}
+		i++;
+	}
+	cout << "Sum of even number =" << summa;*/
+
+	/*4.Вводяться 10 чисел.Знайти суму 
+	та середнє арифметичне цих чисел. - while*/
+
+	/*int i4 = 0;
+	int number4;
+	int summa4 = 0;
+	int average4 = 0;
+	while (i4 < 10)
+	{
+		cout << "Enter " << i4 + 1 << " number : ";
+		cin >> number4;
+		summa4 += number4;
+		i4++;
+	}
+	average4 = summa4 / 10;
+	cout << "Summa = " << summa4 << "; Average = " << average4 << endl;*/
+
+	//5.Вивести на екран цілі числа : 100 99 98 97...3 2 1. - do..while
+
+
+
+
+
+
+
+}
