1a565ec31091487e0621703421bdf1b3a9dc0bbc
Onoka_nd96@student.itstep.org <Onoka_nd96@student.itstep.org>
2025-03-26 15:58:20 +0000

    Lab 17

---
+++ 17_Dynamic_array_Dynamic_memory/17_Dynamic_array_Dynamic_memory.cpp
@@ -0,0 +1,82 @@
+﻿#include <iostream>
+using namespace std;
+
+		/*3. Створити функції для роботи з динамічним масивом
+		- створення  динамічного масиву заданого розміру, розмір приходить як параметр функції
+		- заповнення масиву випадковими числами + виконано
+		- виведення масиву + виконано
+		- доповнення масиву новим елементом(на початок)
+		- доповнення масиву новим елементом(у хвіст масиву) + виконано*/
+
+int* createArr(int size) 
+{
+	int* arr = new int [size] {};
+	return arr;
+}
+
+void fillArrayWithRand(int * & arr, int size)
+{
+	static bool firstTime = true;
+	if (firstTime) {
+		firstTime = false;
+		srand(unsigned(time(0)));
+	}
+	for (int i = 0; i < size; i++)
+	{
+		arr[i] = rand() % 100;
+	}
+}
+
+void printArray(const int* arr, int size)
+{
+	cout << "Array: ";
+	for (int i = 0; i < size; i++)
+	{
+		cout << "\t" << arr[i];
+	}
+}
+
+
+void addToFront(int element, int * & arr, int& size)
+{
+	++size;
+	int* tmp = new int[size]; 
+
+	for (int i = 0; i < size-1; i++)
+	{
+		tmp[i+1] = arr[i];
+	}
+	
+	tmp[0] = element;
+		
+	delete[] arr;
+		
+	arr = tmp;
+}
+
+
+void addToEnd(int element, int*& arr, int& size)
+{
+	++size; 
+	int* tmp = new int[size]; 
+
+	
+	for (int i = 0; i < size - 1; i++)
+	{
+		tmp[i] = arr[i];
+	}
+	
+	tmp[size - 1] = element;
+
+	
+	delete[] arr;
+
+	
+	arr = tmp;
+}
+
+
+
+
+
+
---
+++ 17_Dynamic_array_Dynamic_memory/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int* createArr(int size);
+void fillArrayWithRand(int*& arr, int size);
+void printArray(const int* arr, int size);
+void addToFront(int element, int*& arr, int& size);
+void addToEnd(int element, int*& arr, int& size);
+
+
+void main() {
+
+	int size;
+	cout << "Enter size:  ";
+	cin >> size;
+
+	int* arr = createArr(size);
+	fillArrayWithRand(arr, size);
+	printArray(arr, size);
+
+
+
+
+
+
+}
