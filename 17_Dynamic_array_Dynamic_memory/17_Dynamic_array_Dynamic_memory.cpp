7bdb9241e37d58c21aae240cd756d174de3a1e09
OlexandrOnokalo <Onoka_nd96@student.itstep.org>
2025-03-27 12:52:05 +0200

    HW 17_Dynamic_array_Dynamic_memory DONE

--- 17_Dynamic_array_Dynamic_memory/17_Dynamic_array_Dynamic_memory.cpp
+++ 17_Dynamic_array_Dynamic_memory/17_Dynamic_array_Dynamic_memory.cpp
@@ -6,7 +6,11 @@ using namespace std;
 		- заповнення масиву випадковими числами + виконано
 		- виведення масиву + виконано
 		- доповнення масиву новим елементом(на початок)
-		- доповнення масиву новим елементом(у хвіст масиву) + виконано*/
+		- доповнення масиву новим елементом(у хвіст масиву) + виконано
+		- вилучення із  масиву останнього елементу
+		- вилучення із  масиву першого елементу
+		- вилучення елемента за вказаним індексом
+		- вставки нового елемента за вказаним індексом */
 
 int* createArr(int size) 
 {
@@ -16,14 +20,9 @@ int* createArr(int size)
 
 void fillArrayWithRand(int * & arr, int size)
 {
-	static bool firstTime = true;
-	if (firstTime) {
-		firstTime = false;
-		srand(unsigned(time(0)));
-	}
-	for (int i = 0; i < size; i++)
+    for (int i = 0; i < size; i++)
 	{
-		arr[i] = rand() % 100;
+		arr[i] = ((rand() % 20) - 10);;
 	}
 }
 
@@ -32,8 +31,9 @@ void printArray(const int* arr, int size)
 	cout << "Array: ";
 	for (int i = 0; i < size; i++)
 	{
-		cout << "\t" << arr[i];
+		cout << " " << arr[i];
 	}
+    cout << endl;
 }
 
 
@@ -73,6 +73,178 @@ void addToEnd(int element, int*& arr, int& size)
 
 	
 	arr = tmp;
+}
+
+void removeLast(int*& arr, int& size) 
+{
+    if (size <= 0) {
+        cout << "Error" << endl;
+        return;
+    }
+    --size;
+    int* tmp = new int[size];
+    for (int i = 0; i < size; i++) {
+        tmp[i] = arr[i];
+    }
+    delete[] arr;
+    arr = tmp;
+}
+
+
+void removeFirst(int*& arr, int& size) 
+{
+    if (size <= 0) 
+    {
+        cout << "Error" << endl;
+        return;
+    }
+    --size;
+    int* tmp = new int[size];
+    for (int i = 0; i < size; i++) {
+        tmp[i] = arr[i + 1];
+    }
+    delete[] arr;
+    arr = tmp;
+}
+
+
+void removeAt(int*& arr, int& size, int index) 
+{
+    if (index < 0 || index >= size)
+    {
+        cout << "Error" << endl;
+        return;
+    }
+    --size;
+    int* tmp = new int[size];
+    for (int i = 0, j = 0; i < size + 1; i++) {
+        if (i != index) {
+            tmp[j++] = arr[i];
+        }
+    }
+    delete[] arr;
+    arr = tmp;
+}
+
+
+void insertAt(int*& arr, int& size, int index, int element)
+{
+    if (index < 0 || index > size) {
+        cout << "Error" << endl;
+        return;
+    }
+    ++size;
+    int* tmp = new int[size];
+    for (int i = 0, j = 0; i < size; i++) {
+        if (i == index) {
+            tmp[i] = element;
+        }
+        else {
+            tmp[i] = arr[j++];
+        }
+    }
+    delete[] arr;
+    arr = tmp;
+}
+
+
+        /*Додаткові завдання(за бажанням)*/
+
+    /*5. Визначити функцію видалення усіх від'ємних елементів із динамічного масиву. */
+
+void removeNegatives(int*& arr, int& size) 
+{
+    int newSize = 0;
+    for (int i = 0; i < size; i++) {
+        if (arr[i] >= 0) newSize++;
+    }
+    int* tmp = new int[newSize];
+    for (int i = 0, j = 0; i < size; i++) {
+        if (arr[i] >= 0) {
+            tmp[j++] = arr[i];
+        }
+    }
+    size = newSize;
+    delete[] arr;
+    arr = tmp;
+}
+
+    /*6. Створити функцію, яка дозволяє додати блок елементів у кінець  масиву.*/
+
+void addBlock(int*& arr, int& size, const int* block, int sizeBlock) 
+{
+    int newSize = size + sizeBlock;
+    int* tmp = new int[newSize];
+    for (int i = 0; i < size; i++) {
+        tmp[i] = arr[i];
+    }
+    for (int i = 0; i < sizeBlock; i++) {
+        tmp[size + i] = block[i];
+    }
+    size = newSize;
+    delete[] arr;
+    arr = tmp;
+}
+
+    /*7.  Створити функцію, яка дозволяє вилучити  блок елементів, починаючи із  вказаного індекса вказаної  довжини*/
+
+void removeBlock(int*& arr, int& size, int index, int length) 
+{
+    if (index < 0 || index >= size || length <= 0) {
+        cout << "Error" << endl;
+        return;
+    };
+    if (index + length > size) length = size - index;
+    int newSize = size - length;
+    int* tmp = new int[newSize];
+    for (int i = 0, j = 0; i < size; i++) {
+        if (i < index || i >= index + length) {
+            tmp[j++] = arr[i];
+        }
+    }
+    size = newSize;
+    delete[] arr;
+    arr = tmp;
+}
+
+
+
+    /*8. Дано два масиви : А[M] і B[N](M і N вводяться з клавіатури).
+    Необхідно створити третій масив
+мінімально можливого розміру, 
+у якому потрібно зібрати спільні 
+елементи двох масивів без повторень.*/
+
+
+int* intersect(const int* a, int sizeA, const int* b, int sizeB, int& size) 
+{
+    int tempSize = sizeA < sizeB ? sizeA : sizeB;
+    int* temp = new int[tempSize];
+    size = 0;
+
+    for (int i = 0; i < sizeA; i++) {
+        for (int j = 0; j < sizeB; j++) {
+            if (a[i] == b[j]) {
+                bool found = false;
+                for (int k = 0; k < size; k++) {
+                    if (temp[k] == a[i]) {
+                        found = true;
+                        break;
+                    }
+                }
+                if (!found) {
+                    temp[size++] = a[i];
+                }
+            }
+        }
+    }
+
+    int* result = new int[size];
+    for (int i = 0; i < size; i++) {
+        result[i] = temp[i];
+    }
+    delete[] temp;
+    return result;
 }
 
 
--- 17_Dynamic_array_Dynamic_memory/main.cpp
+++ 17_Dynamic_array_Dynamic_memory/main.cpp
@@ -6,24 +6,114 @@ void fillArrayWithRand(int*& arr, int size);
 void printArray(const int* arr, int size);
 void addToFront(int element, int*& arr, int& size);
 void addToEnd(int element, int*& arr, int& size);
+void removeLast(int*& arr, int& size);
+void removeFirst(int*& arr, int& size);
+void removeAt(int*& arr, int& size, int index);
+void insertAt(int*& arr, int& size, int index, int element);
+void removeNegatives(int*& arr, int& size);
+void addBlock(int*& arr, int& size, const int* block, int sizeBlock);
+void removeBlock(int*& arr, int& size, int index, int length);
+int* intersect(const int* a, int sizeA, const int* b, int sizeB, int& size);
+
+
 
 
 void main() {
-
+	cout << "Task 3: Dynamic array" << endl;
 	int size;
-	cout << "Enter size:  ";
+	cout << "Enter size of array:  ";
 	cin >> size;
 
+	srand(time(0));
 	int* arr = createArr(size);
 	fillArrayWithRand(arr, size);
 	printArray(arr, size);
 	int element;
 	cout << "Enter element to add at front: ";
 	cin >> element;
+	addToFront(element, arr, size);
+	printArray(arr, size);
+	cout << "Enter element to add to end: ";
+	cin >> element;
+	addToEnd(element, arr, size);
+	printArray(arr, size);
+	cout << "Remove last: " << endl;
+	removeLast(arr, size);
+	printArray(arr, size);
+	cout << "Remove first: " << endl;
+	removeFirst(arr, size);
+	printArray(arr, size);
+	cout << "Enter index of element to remove: ";
+	int index;
+	cin >> index;
+	removeAt(arr, size, index);
+	printArray(arr, size);
+	cout << "Enter index of element to add: ";
+	cin >> index;
+	cout << "Enter element to add: ";
+	cin >> element;
+	insertAt(arr, size, index, element);
+	printArray(arr, size);
 	
+	/*5. Визначити функцію видалення усіх від'ємних елементів із динамічного масиву. */
+	cout << "Task 5: Removing negative elements" << endl;
+	cout << "Enter size of array:  ";
+	cin >> size;
+
+	arr = createArr(size);
+	srand(time(0) + 3);
+	fillArrayWithRand(arr, size);
+	printArray(arr, size);
+	removeNegatives(arr, size);
+	cout << "Result: " << endl;
+	printArray(arr, size);
+
+	/*6. Створити функцію, яка дозволяє додати блок елементів у кінець  масиву.*/
+
+	cout << "Task 6: Adding a block to the end of array" << endl;
+	int sizeBlock;
+	cout << "Enter size of block:  ";
+	cin >> sizeBlock;
+	int* block = createArr(sizeBlock);
+	srand(time(0) + 15);
+	fillArrayWithRand(block, sizeBlock);
+	printArray(block,sizeBlock);
+	addBlock(arr, size, block, sizeBlock);
+	cout << "Result: " << endl;
+	printArray(arr, size);
+
+	/*7.  Створити функцію, яка дозволяє вилучити  блок елементів, починаючи із  вказаного індекса вказаної  довжини*/
+	cout << "Task 7: Removing a block from the array" << endl;
+	int	length;
+	cout << "Enter index of start remove: ";
+	cin >> index;
+	cout << "Enter length:  ";
+	cin >> length;
+	removeBlock(arr, size, index, length);
+	cout << "Result: " << endl;
+	printArray(arr, size);
+
+	cout << "Task 8: Finding intersection of two arrays" << endl;
+	int sizeA;
+	int sizeB;
+	cout << "Enter numbers of element fot array one, then for array two:  " << endl;
+	cin >> sizeA>> sizeB;
+	int* A = createArr(sizeA);
+	int* B = createArr(sizeB);
+	srand(time(0) + 5);
+	fillArrayWithRand(A, sizeA);
+	srand(time(0) + 10);
+	fillArrayWithRand(B, sizeB);
+	printArray(A,sizeA);
+	printArray(B,sizeB);
+	arr = intersect(A,sizeA,B,sizeB,size);
+	cout << "Result: " << endl;
+	printArray(arr,size);
+
+	delete[] arr;
+	delete[] block;
 
 
-
-
+	
 
 }
