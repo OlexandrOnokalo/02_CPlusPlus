51c2e79bbd4a7b1291e4b20739403271e1f976cd
Onoka_nd96@student.itstep.org <Onoka_nd96@student.itstep.org>
2025-04-07 16:56:10 +0000

    LAB 23_IntroOOP

---
+++ 23_IntroToOOP/23_IntroToOOP_LAB.cpp
@@ -0,0 +1,66 @@
+﻿
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+    /*Завдання №2
+Реалізуйте клас «Точка».Необхідно зберігати координати
+x, y, z в змінних - членах класу.Реалізуйте функції - члени
+класу для введення даних, виведення даних, реалізуйте
+аксесор для доступу до змінних - членів, реалізуйте збе -
+реження в файл і завантаження даних з файлу.*/
+
+class point
+{
+    double x, y, z;
+public:
+    void enter() {
+        cout << "Enter x: ";
+        cin >> x;
+        cout << "Enter y: ";
+        cin >> y;
+        cout << "Enter x: ";
+        cin >> z;
+    }
+    void print() {
+        cout << "x: " << x << endl;
+        cout << "y: " << y << endl;
+        cout << "z: " << z << endl;
+    }
+    void setX(double x)
+    {
+        this->x = x;
+    }
+    void setY(double Y)
+    {
+        this->y = y;
+    }void setZ(double xz)
+    {
+        this->z = z;
+    }
+    double getX() {
+        return x;
+    }
+    double getY() {
+        return y;
+    }
+    double getZ() {
+        return z;
+    }
+    void saveToFile() {
+
+    }
+
+};
+
+
+
+
+int main()
+{
+
+    
+}
+
