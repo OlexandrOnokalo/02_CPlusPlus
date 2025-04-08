7bb2bdebc70dd0795d29df9e900eddb4addbdb20
OlexandrOnokalo <Onoka_nd96@student.itstep.org>
2025-04-08 13:34:17 +0300

    HW 22_Nested structs DONE

--- 22_Nested structs_Dynamic_array_struts/22_Nested structs_Dynamic_array_struts.cpp
+++ 22_Nested structs_Dynamic_array_struts/22_Nested structs_Dynamic_array_struts.cpp
@@ -1,118 +1,118 @@
-#include <iostream>
-#include <fstream>
-#include <cstring>
-#include <string>
-using namespace std;
-
-const int NAME_SIZE = 100;
-
-struct Marks {
-    int Python;
-    int HTML;
-    int Cpp;
-};
-
-
-struct Student {
-    int id;
-    string Name;
-    Marks marks;
-};
-
-void printMarks(const Marks& marks) {
-    cout << "Python: " << marks.Python << endl;
-    cout << "HTML: " << marks.HTML << endl;
-    cout << "C++: " << marks.Cpp << endl;
-
-}
-
-void intputMarks(Marks& marks) {
-    cout << "Enter Python mark: ";
-    cin >> marks.Python;
-    cout << "Enter HTML mark: ";
-    cin >> marks.HTML;
-    cout << "Enter C++ mark: ";
-    cin >> marks.Cpp;
-}
-
-
-void printStudent(const Student& stud) {
-    cout << "ID: " << stud.id << endl;
-    cout << "Name: " << stud.Name << endl;
-    printMarks(stud.marks);
-}
-void intputStudent(Student& stud) {
-    cout << "Enter id: ";
-    cin >> stud.id;
-    cout << "Enter Name: ";
-    cin >> stud.Name;
-    intputMarks(stud.marks);
-}
-
-void writeToFile(const Student& stud, string path = "stud.txt")
-{
-    ofstream file;
-    file.open(path);
-    
-    if (!file) 
-    {
-        cout << "Error file " << path << endl;
-        return;
-    }
-    file << stud.id << endl;
-    file << stud.Name << endl;
-    file << stud.marks.Python << endl;
-    file << stud.marks.HTML << endl;
-    file << stud.marks.Cpp << endl;
-
-
-}
-void readFromFile(Student& stud, string path = "stud.txt")
-{
-    ifstream file;
-    file.open(path);
-    if (!file) 
-    {
-        cout << "Error file " << path << endl;
-        return;
-    }
-    
-    file >> stud.id;
-    file.ignore(1);
-    getline(file, stud.Name);
-    file >> stud.marks.Python;
-    file >> stud.marks.HTML;
-    file >> stud.marks.Cpp;
-    
-}
-
-
-
-int main()
-{
-    int capacity = 10;
-    int actualSize = 0;
-    Student stud1;
-    Student stud2;
-    
-    cout << "Enter stud info: " << endl;
-    intputStudent(stud1);
-    cout << endl;
-    printStudent(stud1);
-    cout << "Save to file...\n" << endl;
-    writeToFile(stud1);
-    readFromFile(stud2);
-    cout << "Loaded from file:\n" << endl;
-    printStudent(stud2);
-
-
-    int capacity = 10;
-    int actualSize = 0;
-    Student* students = new Student[capacity];
-
-
-
-
-
-}
+//#include <iostream>
+//#include <fstream>
+//#include <cstring>
+//#include <string>
+//using namespace std;
+//
+//const int NAME_SIZE = 100;
+//
+//struct Marks {
+//    int Python;
+//    int HTML;
+//    int Cpp;
+//};
+//
+//
+//struct Student {
+//    int id;
+//    string Name;
+//    Marks marks;
+//};
+//
+//void printMarks(const Marks& marks) {
+//    cout << "Python: " << marks.Python << endl;
+//    cout << "HTML: " << marks.HTML << endl;
+//    cout << "C++: " << marks.Cpp << endl;
+//
+//}
+//
+//void intputMarks(Marks& marks) {
+//    cout << "Enter Python mark: ";
+//    cin >> marks.Python;
+//    cout << "Enter HTML mark: ";
+//    cin >> marks.HTML;
+//    cout << "Enter C++ mark: ";
+//    cin >> marks.Cpp;
+//}
+//
+//
+//void printStudent(const Student& stud) {
+//    cout << "ID: " << stud.id << endl;
+//    cout << "Name: " << stud.Name << endl;
+//    printMarks(stud.marks);
+//}
+//void intputStudent(Student& stud) {
+//    cout << "Enter id: ";
+//    cin >> stud.id;
+//    cout << "Enter Name: ";
+//    cin >> stud.Name;
+//    intputMarks(stud.marks);
+//}
+//
+//void writeToFile(const Student& stud, string path = "stud.txt")
+//{
+//    ofstream file;
+//    file.open(path);
+//    
+//    if (!file) 
+//    {
+//        cout << "Error file " << path << endl;
+//        return;
+//    }
+//    file << stud.id << endl;
+//    file << stud.Name << endl;
+//    file << stud.marks.Python << endl;
+//    file << stud.marks.HTML << endl;
+//    file << stud.marks.Cpp << endl;
+//
+//
+//}
+//void readFromFile(Student& stud, string path = "stud.txt")
+//{
+//    ifstream file;
+//    file.open(path);
+//    if (!file) 
+//    {
+//        cout << "Error file " << path << endl;
+//        return;
+//    }
+//    
+//    file >> stud.id;
+//    file.ignore(1);
+//    getline(file, stud.Name);
+//    file >> stud.marks.Python;
+//    file >> stud.marks.HTML;
+//    file >> stud.marks.Cpp;
+//    
+//}
+//
+//
+//
+//int main()
+//{
+//    int capacity = 10;
+//    int actualSize = 0;
+//    Student stud1;
+//    Student stud2;
+//    
+//    cout << "Enter stud info: " << endl;
+//    intputStudent(stud1);
+//    cout << endl;
+//    printStudent(stud1);
+//    cout << "Save to file...\n" << endl;
+//    writeToFile(stud1);
+//    readFromFile(stud2);
+//    cout << "Loaded from file:\n" << endl;
+//    printStudent(stud2);
+//
+//
+//    int capacity = 10;
+//    int actualSize = 0;
+//    Student* students = new Student[capacity];
+//
+//
+//
+//
+//
+//}
 
---
+++ 22_Nested structs_Dynamic_array_struts/Source.cpp
@@ -0,0 +1,195 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+
+struct Point {
+	int x;
+	int y;
+};
+
+
+void printPoint(const Point& point) {
+	cout << "Point: (" << point.x << ", " << point.y << ")" << endl;
+}
+
+
+void inputPoint(Point& point) {
+	cout << "Enter x: ";
+	cin >> point.x;
+	cout << "Enter y: ";
+	cin >> point.y;
+}
+
+
+void printPointsArray(const Point* pointsArray, int size) {
+	for (int i = 0; i < size; ++i) {
+		printPoint(pointsArray[i]);
+	}
+}
+
+
+void writePointToFileStream(const Point& point, ofstream& file) {
+	file << point.x << "\t" << point.y << endl;
+}
+
+
+void readPointFromFileStream(Point& point, ifstream& file) {
+	file >> point.x >> point.y;
+}
+
+
+struct Marks {
+	int Python;
+	int HTML;
+	int Cpp;
+};
+
+
+struct Student {
+	int id;
+	string Name;
+	Marks marks;
+};
+
+
+void viewStudents(Student* group, int size) {
+	for (int i = 0; i < size; ++i) {
+		cout << "ID: " << group[i].id << ", Name: " << group[i].Name
+			<< ", Python: " << group[i].marks.Python
+			<< ", HTML: " << group[i].marks.HTML
+			<< ", C++: " << group[i].marks.Cpp << endl;
+	}
+}
+
+
+void showTopStudents(Student* group, int size) {
+	for (int i = 0; i < size; ++i) {
+		double avg = (group[i].marks.Python + group[i].marks.HTML + group[i].marks.Cpp) / 3.0;
+		if (avg > 10) {
+			cout << "Top student: " << group[i].Name << " (Average: " << avg << ")" << endl;
+		}
+	}
+}
+
+
+void addStudent(Student* group, int& actualSize) {
+	cout << "Enter ID: ";
+	cin >> group[actualSize].id;
+	cout << "Enter name: ";
+	cin.ignore();
+	getline(cin, group[actualSize].Name);
+	cout << "Enter Python mark: ";
+	cin >> group[actualSize].marks.Python;
+	cout << "Enter HTML mark: ";
+	cin >> group[actualSize].marks.HTML;
+	cout << "Enter C++ mark: ";
+	cin >> group[actualSize].marks.Cpp;
+	actualSize++;
+}
+
+
+void writeStudentsToFile(Student* group, int size, const string& filename) {
+	ofstream file(filename);
+	file << size << endl;
+	for (int i = 0; i < size; ++i) {
+		file << group[i].id << "\t" << group[i].Name << "\t"
+			<< group[i].marks.Python << "\t"
+			<< group[i].marks.HTML << "\t"
+			<< group[i].marks.Cpp << endl;
+	}
+	file.close();
+}
+
+
+Student* loadStudentsFromFile(int& actualSize, int& capacity, const string& filename) {
+	ifstream file(filename);
+	file >> actualSize;
+	capacity = actualSize + 5; 
+	Student* group = new Student[capacity];
+	for (int i = 0; i < actualSize; ++i) {
+		file >> group[i].id;
+		file >> ws;
+		getline(file, group[i].Name, '\t');
+		file >> group[i].marks.Python >> group[i].marks.HTML >> group[i].marks.Cpp;
+	}
+	file.close();
+	return group;
+}
+
+
+
+int main()
+{
+	
+	const int SIZE = 5;
+	Point points[SIZE]{};
+
+	
+	for (int i = 0; i < SIZE; ++i) {
+		cout << "Enter point #" << i + 1 << ":" << endl;
+		inputPoint(points[i]);
+	}
+
+	ofstream file("points.txt"); 
+
+	
+	for (int i = 0; i < SIZE; ++i) {
+		writePointToFileStream(points[i], file);
+	}
+
+	file.close();
+
+	ifstream file2("points.txt");
+
+	
+	int count = 0;
+	int tempX, tempY;
+	while (file2 >> tempX >> tempY) {
+		count++;
+	}
+	file2.clear();
+	file2.seekg(0);
+
+	Point* readPoint = new Point[count];
+
+	for (int i = 0; i < count; ++i) {
+		readPointFromFileStream(readPoint[i], file2);
+	}
+
+	cout << "\nPoints loaded from file:" << endl;
+	printPointsArray(readPoint, count);
+	delete[] readPoint;
+	file2.close();
+
+	
+	int capacity = 10;
+	int actualSize = 0;
+	Student* group = new Student[capacity];
+
+	
+	addStudent(group, actualSize);
+	addStudent(group, actualSize);
+
+	cout << "\nAll students:" << endl;
+	viewStudents(group, actualSize);
+
+	cout << "\nTop students:" << endl;
+	showTopStudents(group, actualSize);
+
+	writeStudentsToFile(group, actualSize, "students.txt");
+
+	
+	int loadedSize, loadedCapacity;
+	Student* loadedGroup = loadStudentsFromFile(loadedSize, loadedCapacity, "students.txt");
+
+	cout << "\nStudents loaded from file:" << endl;
+	viewStudents(loadedGroup, loadedSize);
+
+	delete[] group;
+	delete[] loadedGroup;
+
+	return 0;
+}
