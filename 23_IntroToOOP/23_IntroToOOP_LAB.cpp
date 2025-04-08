9bf8ffc5a3a58c891af5bf13e71d54db6b39bbf1
OlexandrOnokalo <Onoka_nd96@student.itstep.org>
2025-04-08 14:36:30 +0300

    HW 23_IntroToOOP DONE

--- 23_IntroToOOP/23_IntroToOOP_LAB.cpp
+++ 23_IntroToOOP/23_IntroToOOP_LAB.cpp
@@ -1,66 +1,152 @@
-﻿
-
-#include <iostream>
+﻿#include <iostream>
+#include <fstream>
 #include <string>
 using namespace std;
 
+    /*Завдання №2 Реалізуйте клас «Точка».Необхідно зберігати 
+координати x, y, z в змінних - членах класу.Реалізуйте функції - 
+члени класу для введення даних, виведення даних, реалізуйте аксесор 
+для доступу до змінних - членів, реалізуйте збереження в файл і завантаження даних з файлу.*/
 
-    /*Завдання №2
-Реалізуйте клас «Точка».Необхідно зберігати координати
-x, y, z в змінних - членах класу.Реалізуйте функції - члени
-класу для введення даних, виведення даних, реалізуйте
-аксесор для доступу до змінних - членів, реалізуйте збе -
-реження в файл і завантаження даних з файлу.*/
-
-class point
-{
+class Point {
+private:
+    
     double x, y, z;
+
 public:
-    void enter() {
-        cout << "Enter x: ";
-        cin >> x;
-        cout << "Enter y: ";
-        cin >> y;
-        cout << "Enter x: ";
-        cin >> z;
+    
+    void input() {
+        cout << "Enter coordinates of the point (x, y, z): ";
+        cin >> x >> y >> z;
     }
-    void print() {
-        cout << "x: " << x << endl;
-        cout << "y: " << y << endl;
-        cout << "z: " << z << endl;
+
+    
+    void output()  {
+        cout << "Point coordinates: (" << x << ", " << y << ", " << z << ")" << endl;
     }
-    void setX(double x)
-    {
-        this->x = x;
-    }
-    void setY(double Y)
-    {
-        this->y = y;
-    }void setZ(double xz)
-    {
-        this->z = z;
-    }
-    double getX() {
-        return x;
-    }
-    double getY() {
-        return y;
-    }
-    double getZ() {
-        return z;
-    }
-    void saveToFile() {
+
+    
+    double getX()  { return x; }
+    double getY()  { return y; }
+    double getZ()  { return z; }
+
+    
+    void saveToFile(const string& filename)  {
+        ofstream outFile(filename);
+        outFile << x << " " << y << " " << z << endl;
+        outFile.close();
+        cout << "Point data saved to file." << endl;
+        
 
     }
 
+     void loadFromFile(const string& filename) {
+        ifstream inFile(filename);
+        inFile >> x >> y >> z;
+        inFile.close();
+        cout << "Point data loaded from file." << endl;
+
+        
+    }
+};
+
+    /*Завдання №1 Реалізуйте клас «Студент».Необхідно зберігати 
+в змінних - членах класу : 
+ПІБ, дату народження, контактний телефон, 
+місто, країну, назву навчального закладу, 
+місто та країну(де знаходиться навчальний заклад), 
+номер групи.Реалізуйте функції - 
+члени класу для введення даних, виведення даних, 
+реалізуйте аксесор для доступу до окремих змінних - членів.*/
+
+class Student {
+private:
+    string name;
+    string dateOfBirth;
+    string phoneNumber;
+    string city;
+    string country;
+    string institutionName;
+    string institutionCity;
+    string institutionCountry;
+    int groupNumber;
+
+public:
+    void input() {
+        cout << "Enter student's name: ";
+        cin.ignore();
+        getline(cin, name);
+        cout << "Enter student's date of birth (DD/MM/YYYY): ";
+        getline(cin, dateOfBirth);
+        cout << "Enter student's phone number: ";
+        getline(cin, phoneNumber);
+        cout << "Enter student's city: ";
+        getline(cin, city);
+        cout << "Enter student's country: ";
+        getline(cin, country);
+        cout << "Enter student's educational institution name: ";
+        getline(cin, institutionName);
+        cout << "Enter institution city: ";
+        getline(cin, institutionCity);
+        cout << "Enter institution country: ";
+        getline(cin, institutionCountry);
+        cout << "Enter student's group number: ";
+        cin >> groupNumber;
+    }
+
+    void output()  {
+        cout << "Student's Name: " << name << endl;
+        cout << "Date of Birth: " << dateOfBirth << endl;
+        cout << "Phone Number: " << phoneNumber << endl;
+        cout << "City: " << city << endl;
+        cout << "Country: " << country << endl;
+        cout << "Educational Institution: " << institutionName << endl;
+        cout << "Institution City: " << institutionCity << endl;
+        cout << "Institution Country: " << institutionCountry << endl;
+        cout << "Group Number: " << groupNumber << endl;
+    }
+
+
+    string getName()  { return name; }
+
+
+    string getDateOfBirth()  { return dateOfBirth; }
+
+
+    string getPhoneNumber()  { return phoneNumber; }
+
+
+    string getCity()  { return city; }
+
+
+    string getCountry()  { return country; }
+
+
+    string getInstitutionName()  { return institutionName; }
+
+
+    string getInstitutionCity()  { return institutionCity; }
+
+
+    string getInstitutionCountry()  { return institutionCountry; }
+
+
+    int getGroupNumber()  { return groupNumber; }
 };
 
 
+int main() {
+
+    Point p1;
+    p1.input();  
+    p1.output(); 
+    p1.saveToFile("point.txt");
+    p1.loadFromFile("point.txt"); 
 
 
-int main()
-{
+    Student s1;
+    s1.input();  
+    s1.output(); 
 
-    
+    return 0;
 }
-
