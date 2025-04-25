6ecd6ac501de3c8734713d84c31deff78b3ea1ee
OlexandrOnokalo <Onoka_nd96@student.itstep.org>
2025-04-25 18:56:53 +0300

    HW 28 DONE

--- 28_OverloadOperators/28_OverloadOperators.cpp
+++ 28_OverloadOperators/28_OverloadOperators.cpp
@@ -27,63 +27,138 @@ using namespace std;
 == !=		перевіряється чи класи мають не / однаковий час
 Використайте explicit для деяких конструкторів*/
 
-class Time
-{
+class Time {
     int hours;
     int minutes;
     int seconds;
-    int total;
+
 public:
-    Time()
-    {
+    Time() {
         hours = 0;
         minutes = 0;
         seconds = 0;
-        total = 0;
     }
-    Time(int hours, int minutes,int seconds)
-    {
-        this->hours = hours;
-        this->minutes = minutes;
-        this->seconds = seconds;
-        total = 0;
+
+    Time(int h, int m, int s) {
+        hours = h;
+        minutes = m;
+        seconds = s;
     }
-    Time(int seconds)
-    {
-        this->hours = seconds / 3600;
-        this->minutes = seconds / 60 % 60;
-        this->seconds = seconds % 60;
-        total = 0;
+
+    explicit Time(int totalSeconds) {
+        hours = totalSeconds / 3600;
+        minutes = (totalSeconds / 60) % 60;
+        seconds = totalSeconds % 60;
     }
-    Time operator +(Time& other) const
-    {
-        Time res((*this).totalSeconds() + other.totalSeconds());
-        return res;
+
+    void Print() const {
+        cout << (hours < 10 ? "0" : "") << hours << ":"
+            << (minutes < 10 ? "0" : "") << minutes << ":"
+            << (seconds < 10 ? "0" : "") << seconds << endl;
     }
-    int totalSeconds() {
+
+    int TotalSeconds() const {
         return hours * 3600 + minutes * 60 + seconds;
     }
-    void ToHMS() {
-        this->hours = total / 3600;
-        this->minutes = total / 60 % 60;
-        this->seconds = total % 60;
+
+    void Tick() {
+        int total = TotalSeconds() + 1;
+        hours = total / 3600;
+        minutes = (total / 60) % 60;
+        seconds = total % 60;
     }
 
+    Time operator+(const Time& other) const {
+        int total = this->TotalSeconds() + other.TotalSeconds();
+        return Time(total);
+    }
 
+    Time operator-(const Time& other) const {
+        int total = this->TotalSeconds() - other.TotalSeconds();
+        if (total < 0) total = 0;
+        return Time(total);
+    }
 
-}
+    Time operator*(int multiplier) const {
+        int total = this->TotalSeconds() * multiplier;
+        return Time(total);
+    }
 
+    Time operator/(int divisor) const {
+        if (divisor == 0) return Time(0);
+        int total = this->TotalSeconds() / divisor;
+        return Time(total);
+    }
 
+    bool operator>(const Time& other) const {
+        return this->TotalSeconds() > other.TotalSeconds();
+    }
 
+    bool operator<(const Time& other) const {
+        return this->TotalSeconds() < other.TotalSeconds();
+    }
 
+    bool operator>=(const Time& other) const {
+        return this->TotalSeconds() >= other.TotalSeconds();
+    }
 
+    bool operator<=(const Time& other) const {
+        return this->TotalSeconds() <= other.TotalSeconds();
+    }
 
+    bool operator==(const Time& other) const {
+        return this->TotalSeconds() == other.TotalSeconds();
+    }
 
+    bool operator!=(const Time& other) const {
+        return this->TotalSeconds() != other.TotalSeconds();
+    }
+};
 
+int main() {
+    cout << "Testing Time class:\n";
 
-void main()
-{
-    
+    Time t1;
+    cout << "Time t1 (default constructor): ";
+    t1.Print();
+
+    Time t2(2, 30, 15);
+    cout << "Time t2 (parameterized constructor): ";
+    t2.Print();
+
+    Time t3(5555);
+    cout << "Time t3 (5555 seconds): ";
+    t3.Print();
+
+    cout << "\nAdding t2 + t3: ";
+    Time t4 = t2 + t3;
+    t4.Print();
+
+    cout << "Subtracting t4 - t2: ";
+    Time t5 = t4 - t2;
+    t5.Print();
+
+    cout << "Multiplying t2 * 2: ";
+    Time t6 = t2 * 2;
+    t6.Print();
+
+    cout << "Dividing t6 / 2: ";
+    Time t7 = t6 / 2;
+    t7.Print();
+
+    cout << "\nTick() t2 + 1 second: ";
+    t2.Tick();
+    t2.Print();
+
+    cout << "\nComparisons:\n";
+    cout << "t2 == t2? " << (t2 == t2) << endl;
+    cout << "t2 != t3? " << (t2 != t3) << endl;
+    cout << "t2 > t3?  " << (t2 > t3) << endl;
+    cout << "t2 < t3?  " << (t2 < t3) << endl;
+    cout << "t2 >= t3? " << (t2 >= t3) << endl;
+    cout << "t2 <= t3? " << (t2 <= t3) << endl;
+
+    return 0;
 }
 
 
