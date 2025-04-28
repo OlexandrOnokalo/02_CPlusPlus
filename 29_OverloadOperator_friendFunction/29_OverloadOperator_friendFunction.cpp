297f5e10dad69668b6246c2d97b37003a7ce1963
OlexandrOnokalo <Onoka_nd96@student.itstep.org>
2025-04-28 20:19:43 +0300

    HW 29 DONE

--- 29_OverloadOperator_friendFunction/29_OverloadOperator_friendFunction.cpp
+++ 29_OverloadOperator_friendFunction/29_OverloadOperator_friendFunction.cpp
@@ -1,4 +1,5 @@
 ﻿#include <iostream>
+#include <string>
 using namespace std;
 
 
@@ -22,13 +23,120 @@ using namespace std;
     !Результат завантажити у вигляді.cpp файла.*/
 
 
-
-
-
-
-
-
-int main()
+class Airplane
 {
-    
+    string model;
+    string type;
+    int passengers;
+    int maxPassengers;
+public:
+    Airplane()
+    {
+        model = "";
+        type = "";
+        passengers = 0;
+        maxPassengers = 0;
+    }
+    Airplane(string model, string type, int passengers, int maxPassengers)
+    {
+        this->model = model;
+        this->type = type;
+        this->passengers = passengers;
+        this->maxPassengers = maxPassengers;
+        if (this->passengers > this->maxPassengers)
+            this->passengers = this->maxPassengers;
+    }
+    friend bool operator==(const Airplane& a1, const Airplane& a2);
+    friend bool operator>(const Airplane& a1, const Airplane& a2);
+
+    Airplane& operator++()
+    {
+        if (passengers < maxPassengers)
+            passengers++;
+        return *this;
+    }
+
+    Airplane& operator--()
+    {
+        if (passengers > 0)
+            passengers--;
+        return *this;
+    }
+
+
+    void operator()(int value)
+    {
+        passengers += value;
+        if (passengers > maxPassengers)
+            passengers = maxPassengers;
+        if (passengers < 0)
+            passengers = 0;
+    }
+
+    void Print() const
+    {
+        cout << "Model: " << model << endl;
+        cout << "Type: " << type << endl;
+        cout << "Passengers: " << passengers << "/" << maxPassengers << endl;
+    }
+};
+
+
+
+bool operator==(const Airplane& a1, const Airplane& a2)
+{
+    return a1.type == a2.type;
+}
+
+bool operator>(const Airplane& a1, const Airplane& a2)
+{
+    return a1.maxPassengers > a2.maxPassengers;
+}
+
+
+
+
+
+
+
+void main()
+{
+    Airplane plane1("Boeing 737", "Passenger", 120, 160);
+    Airplane plane2("Airbus A320", "Passenger", 130, 150);
+    Airplane plane3("Cessna 172", "Private", 3, 4);
+
+    cout << "Plane 1:" << endl;
+    plane1.Print();
+    cout << endl;
+
+    cout << "Plane 2:" << endl;
+    plane2.Print();
+    cout << endl;
+
+    cout << "Plane 3:" << endl;
+    plane3.Print();
+    cout << endl;
+
+    cout << "Check if Plane1 and Plane2 have same type: ";
+    cout << (plane1 == plane2 ? "Yes" : "No") << endl;
+
+    cout << "Check if Plane1 has more max passengers than Plane3: ";
+    cout << (plane1 > plane3 ? "Yes" : "No") << endl;
+
+    cout << endl << "Increasing passengers in Plane1:" << endl;
+    ++plane1;
+    plane1.Print();
+
+    cout << endl << "Decreasing passengers in Plane2 :" << endl;
+    --plane2;
+    plane2.Print();
+
+    cout << endl << "Adding 10 passengers to Plane1 :" << endl;
+    plane1(10);
+    plane1.Print();
+
+  
+
+
+
 }
