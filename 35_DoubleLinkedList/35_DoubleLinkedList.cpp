9d2825e173f6ac4b8a34c09c97d70a64229375bf
OlexandrOnokalo <Onoka_nd96@student.itstep.org>
2025-05-12 18:33:05 +0300

    HW 35 DONE

--- 35_DoubleLinkedList/35_DoubleLinkedList.cpp
+++ 35_DoubleLinkedList/35_DoubleLinkedList.cpp
@@ -31,7 +31,6 @@ struct Vagon
 
 
 
-
 template<typename T>
 class List
 {
@@ -40,63 +39,32 @@ class List
         Node* prev;
         T value;
         Node* next;
-        //c-tors....show()
-        Node(Node* prev, T value, Node* next) :prev(prev), value(value), next(next) {}
+        Node(Node* prev, T value, Node* next) : prev(prev), value(value), next(next) {}
     };
 
 private:
     Node* head;
     Node* tail;
-public:
-    List()
-    {
-        head = nullptr;
-        tail = nullptr;
-    }
-    bool IsEmpty()
-    {
-        return head == nullptr;
-    }
-    void AddToHead(T value)
-    {
-        Node* newNode = new Node(nullptr, value, head);
 
-        if (IsEmpty())
-        {
+public:
+    List() : head(nullptr), tail(nullptr) {}
+
+    bool IsEmpty() const { return head == nullptr; }
+
+    void AddToHead(T value) {
+        Node* newNode = new Node(nullptr, value, head);
+        if (IsEmpty()) {
             head = tail = newNode;
         }
-        head->prev = newNode;
-        head = newNode;
-    }
-    void Print()const
-    {
-        //Node* current = head;//int i = 0;
-        //while (current != nullptr)//i < 10;
-        //{
-        //    cout << current->value << " ";
-        //    current = current->next;//i++;
-        //}
-        for (Node* i = head; i != nullptr; i = i->next)
-        {
-            cout << i->value << " ";
-        }
-        cout << endl;
-    }
-    ~List()
-    {
-        Node* current = nullptr;
-        while (head != nullptr)
-        {
-            current = head;
-            head = head->next;
-            delete current;
+        else {
+            head->prev = newNode;
+            head = newNode;
         }
     }
-    void AddToTail(T value)
-    {
+
+    void AddToTail(T value) {
         Node* newNode = new Node(tail, value, nullptr);
-        if (head == nullptr)
-        {
+        if (IsEmpty()) {
             head = tail = newNode;
         }
         else {
@@ -104,159 +72,185 @@ public:
             tail = newNode;
         }
     }
-    int GetElement(int pos)
-    {
-        Node* current = head;
-        int i = 1;
-        while (current != nullptr)
-        {
-            if (pos == i) return current->value;
-            current = current->next;
-            i++;
-        }
-    }
-    int operator[](int pos)
-    {
-        Node* current = head;
-        int i = 1;
-        while (current != nullptr)
-        {
-            if (pos == i) return current->value;
-            current = current->next;
-            i++;
-        }
-    }
-    void DeleteFromTail()
-    {
-        if (IsEmpty()) return;
-        if (head->next == nullptr)
-        {
-            delete head;
-            head = tail = nullptr;
 
-        }
-        else {
-            tail = tail->prev;
-            delete tail->next;
-            tail->next = nullptr;
-        }
-    }
-    void DeleteFromHead()
-    {
-        if (head == nullptr) return;
+    void DeleteFromHead() {
+        if (IsEmpty()) return;
         Node* temp = head;
         head = head->next;
+        if (head) head->prev = nullptr;
+        else tail = nullptr;
         delete temp;
     }
 
+    void DeleteFromTail() {
+        if (IsEmpty()) return;
+        Node* temp = tail;
+        tail = tail->prev;
+        if (tail) tail->next = nullptr;
+        else head = nullptr;
+        delete temp;
+    }
+
+    void AddAt(int pos, const T& value) {
+        if (pos <= 0) return AddToHead(value);
+        Node* current = head;
+        for (int i = 0; i < pos - 1 && current != nullptr; ++i)
+            current = current->next;
+        if (!current) return AddToTail(value);
+        Node* newNode = new Node(current, value, current->next);
+        if (current->next)
+            current->next->prev = newNode;
+        else
+            tail = newNode;
+        current->next = newNode;
+    }
+
+    void DeleteAt(int pos) {
+        if (IsEmpty()) return;
+        if (pos == 0) return DeleteFromHead();
+        Node* current = head;
+        for (int i = 0; i < pos && current != nullptr; ++i)
+            current = current->next;
+        if (!current) return;
+        if (current->prev)
+            current->prev->next = current->next;
+        if (current->next)
+            current->next->prev = current->prev;
+        if (current == head) head = current->next;
+        if (current == tail) tail = current->prev;
+        delete current;
+    }
+
+    void Print() const {
+        Node* current = head;
+        while (current != nullptr) {
+            cout << current->value;
+            current = current->next;
+        }
+    }
+
+    ~List() {
+        while (!IsEmpty())
+            DeleteFromHead();
+    }
+
+    List(const List& other) : head(nullptr), tail(nullptr) {
+        Node* current = other.head;
+        while (current != nullptr) {
+            AddToTail(current->value);
+            current = current->next;
+        }
+    }
+
+    List& operator=(const List& other) {
+        if (this == &other) return *this;
+        while (!IsEmpty()) DeleteFromHead();
+        Node* current = other.head;
+        while (current != nullptr) {
+            AddToTail(current->value);
+            current = current->next;
+        }
+        return *this;
+    }
 };
-
-
-
 
 struct Vagon {
     int number;
     int places;
     int passengers;
-    Vagon() :number(0), places(0), passengers(0) {}
-    Vagon(int n, int p, int pass) :number(n), places(p), passengers(pass) {}
-    void Show()const
-    {
-        cout << "Vagon number: " << number << endl;
-        cout << "Vagon places: " << places << endl;
-        cout << "Vagon passengers: " << passengers << endl;
+    Vagon() : number(0), places(0), passengers(0) {}
+    Vagon(int n, int p, int pass) : number(n), places(p), passengers(pass) {}
+
+    void Show() const {
+        cout << "Vagon number: " << number << ", places: " << places << ", passengers: " << passengers << endl;
     }
 };
 
-ostream& operator <<(ostream& out, const Vagon& v) {
-    out << "Vagon number: " << v.number << endl;
-    out << "Vagon places: " << v.places << endl;
-    out << "Vagon passengers: " << v.passengers << endl;
+ostream& operator<<(ostream& out, const Vagon& v) {
+    out << "[#" << v.number << ", places: " << v.places << ", pass: " << v.passengers << "] "<< endl;
     return out;
 }
-
-
 
 class Train {
 private:
     string model;
     int vagonCount;
     List<Vagon> vagons;
+
 public:
-    Train()
-    {
-        model = "Hyundai";
-        vagonCount = 0;
+    Train() : model("Hyundai"), vagonCount(0) {}
+    Train(string model) : model(model), vagonCount(0) {}
 
+    Train(const Train& other) {
+        model = other.model;            
+        vagonCount = other.vagonCount;  
+        vagons = other.vagons;          
     }
-    Train(string model)
-    {
-        this->model = model;
-        vagonCount = 0;
-    }
-    /*Train(const Train& other) {
-        this->model = other.model;
-        this->vagonCount = other.vagonCount;
 
-        if (vagonCount > 0) {
-            this->vagons = new Vagon[vagonCount];
-            for (int i = 0; i < vagonCount; ++i) {
-                this->vagons[i] = other.vagons[i];
-            }
-        }
-        else {
-            this->vagons = nullptr;
-        }
-    }*/
-    void AddToHead(const Vagon& vag)
-    {
+    void AddToHead(const Vagon& vag) {
         vagons.AddToHead(vag);
         vagonCount++;
     }
-    void AddToTail(const Vagon& vag)
-    {
+
+    void AddToTail(const Vagon& vag) {
         vagons.AddToTail(vag);
         vagonCount++;
     }
-    void DeleteFromHead()
-    {
+
+    void DeleteFromHead() {
         vagons.DeleteFromHead();
         if (vagonCount > 0) vagonCount--;
     }
-    void DeleteFromTail()
-    {
+
+    void DeleteFromTail() {
         vagons.DeleteFromTail();
         if (vagonCount > 0) vagonCount--;
     }
-    void Show() const
-    {
-        vagons.Print();
-        cout << "Total Points: " << vagonCount << endl;
+
+    void AddAtPosition(int pos, const Vagon& vag) {
+        vagons.AddAt(pos, vag);
+        vagonCount++;
     }
 
+    void DeleteAtPosition(int pos) {
+        vagons.DeleteAt(pos);
+        if (vagonCount > 0) vagonCount--;
+    }
 
+    void Show() const {
+        cout << "Train model: " << model << "\n";
+        cout << "Vagons:\n";
+        vagons.Print();
+        cout << "\nTotal Vagons: " << vagonCount << "\n\n";
+    }
 };
 
+int main() {
+    Train t("Intercity");
 
-
-
-int main()
-{
-    Train t;
     t.AddToHead(Vagon(1, 20, 10));
-    t.AddToTail(Vagon(2, 20, 12));
-    t.AddToTail(Vagon(3, 30, 13));
-    cout << "Initial list of vagons:\n";
+    t.AddToTail(Vagon(2, 25, 15));
+    t.AddToTail(Vagon(3, 30, 20));
+    t.AddAtPosition(0, Vagon(0, 40, 35)); 
+
+    cout << "Initial train:\n";
     t.Show();
-    cout << "Deleting from tail:\n";
-    t.DeleteFromTail();
-    t.Show();
-    cout << "Deleting from Haed:\n";
+
     t.DeleteFromHead();
+    cout << "After deleting from head:\n";
     t.Show();
 
-    
+    t.DeleteFromTail();
+    cout << "After deleting from tail:\n";
+    t.Show();
 
+    t.DeleteAtPosition(1);
+    cout << "After deleting at position 1:\n";
+    t.Show();
 
+    Train copy = t;
+    cout << "Copied train:\n";
+    copy.Show();
 
+    return 0;
 }
