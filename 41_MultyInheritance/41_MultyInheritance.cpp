222a2d515b2d61b4cd246a21242ac822148ec2a3
OlexandrOnokalo <Onoka_nd96@student.itstep.org>
2025-05-22 20:05:08 +0300

    HW 41 DONE

--- 41_MultyInheritance/41_MultyInheritance.cpp
+++ 41_MultyInheritance/41_MultyInheritance.cpp
@@ -1,5 +1,4 @@
-﻿#include <iostream>
-using namespace std;
+﻿
 
     /*1.Створити клас Піца з чисто віртуальним методом
 Приготувати піцу
@@ -38,97 +37,146 @@ class MilanFactory
 2. За бажанням доповнити клас піца та соус інградієнтами*/
 
 #include <iostream>
+#include <string>
 using namespace std;
-class Pizza
-{
+
+
+class Pizza {
 public:
 	virtual void Make_pizza() = 0;
+	virtual ~Pizza() {}
 };
-class MilanPizza :public Pizza
-{
-public:
-	void Make_pizza()override
-	{
-		cout << "I am cooking Milan Pizza" << endl;
-	}
-};
-class GreecePizza :public Pizza
-{
-public:
-	void Make_pizza()override
-	{
-		cout << "I am cooking Greece Pizza" << endl;
-	}
-};
-class Souse
-{
+
+class Souse {
 public:
 	virtual void Make_Souse() = 0;
+	virtual ~Souse() {}
 };
-class MilanSouse :public Souse
-{
-public:
-	void Make_Souse()override
-	{
-		cout << "I am cooking Milan Souse" << endl;
-	}
-};
-class GreeceSous :public Souse
-{
-public:
-	void Make_Souse()override
-	{
-		cout << "I am cooking Greece Souse" << endl;
-	}
-};
-class Factory
-{
+
+class Factory {
 public:
 	virtual Pizza* CreatePiza() = 0;
 	virtual Souse* CreateSouse() = 0;
+	virtual ~Factory() {}
 };
-class MilanFactory :public Factory
-{
+
+
+class MilanPizza : public Pizza {
 public:
-	Pizza* CreatePiza()override
-	{
+	void Make_pizza() override {
+		cout << "I am cooking Milan Pizza with:\n";
+		cout << "- Thin crust\n";
+		cout << "- Mozzarella\n";
+		cout << "- Tomato sauce\n";
+		cout << "- Basil\n\n";
+	}
+};
+
+class MilanSouse : public Souse {
+public:
+	void Make_Souse() override {
+		cout << "I am cooking Milan Souse with:\n";
+		cout << "- Olive oil\n";
+		cout << "- Garlic\n";
+		cout << "- Oregano\n\n";
+	}
+};
+
+
+class GreecePizza : public Pizza {
+public:
+	void Make_pizza() override {
+		cout << "I am cooking Greece Pizza with:\n";
+		cout << "- Thick crust\n";
+		cout << "- Feta cheese\n";
+		cout << "- Olives\n";
+		cout << "- Red onions\n\n";
+	}
+};
+
+class GreeceSous : public Souse {
+public:
+	void Make_Souse() override {
+		cout << "I am cooking Greece Souse with:\n";
+		cout << "- Greek yogurt\n";
+		cout << "- Cucumber\n";
+		cout << "- Garlic\n";
+		cout << "- Dill\n\n";
+	}
+};
+
+
+class MilanFactory : public Factory {
+public:
+	Pizza* CreatePiza() override {
 		return new MilanPizza();
 	}
-	Souse* CreateSouse()override
-	{
+	Souse* CreateSouse() override {
 		return new MilanSouse();
 	}
 };
-class GreeceFactory :public Factory
-{
+
+class GreeceFactory : public Factory {
 public:
-	Pizza* CreatePiza()override
-	{
+	Pizza* CreatePiza() override {
 		return new GreecePizza();
 	}
-	Souse* CreateSouse()override
-	{
+	Souse* CreateSouse() override {
 		return new GreeceSous();
 	}
 };
-int main()
-{
-	Factory* factory = new  MilanFactory();
-	Pizza* pizza = factory->CreatePiza();
-	pizza->Make_pizza();
-	Souse* souse = factory->CreateSouse();
-	souse->Make_Souse();
-	delete factory;
-	delete pizza;
-	delete souse;
-	factory = new GreeceFactory();
-	pizza = factory->CreatePiza();
-	pizza->Make_pizza();
-	souse = factory->CreateSouse();
-	souse->Make_Souse();
-	delete factory;
-	delete pizza;
-	delete souse;
+
+
+void ShowMenu() {
+	cout << "=== Pizza Factory ===\n";
+	cout << "1. Make Milan Pizza and Souse\n";
+	cout << "2. Make Greece Pizza and Souse\n";
+	cout << "0. Exit\n";
+	cout << "Your choice: ";
 }
+
+int main() {
+	int choice;
+	do {
+		ShowMenu();
+		cin >> choice;
+		cout << endl;
+
+		Factory* factory = nullptr;
+		Pizza* pizza = nullptr;
+		Souse* souse = nullptr;
+
+		switch (choice) {
+		case 1:
+			factory = new MilanFactory();
+			break;
+		case 2:
+			factory = new GreeceFactory();
+			break;
+		case 0:
+			cout << "Goodbye!\n";
+			break;
+		default:
+			cout << "Invalid choice. Try again.\n\n";
+			continue;
+		}
+
+		if (factory) {
+			pizza = factory->CreatePiza();
+			souse = factory->CreateSouse();
+
+			pizza->Make_pizza();
+			souse->Make_Souse();
+
+			delete pizza;
+			delete souse;
+			delete factory;
+		}
+
+	} while (choice != 0);
+
+	return 0;
+}
+
 
 
