27180d321bb894fef43bfb01f9c0696cd0c2d690
OlexandrOnokalo <Onoka_nd96@student.itstep.org>
2025-05-29 19:34:26 +0300

    HW 44 DONE

--- 44_02_ZooSaveToFile/44_02_ZooSaveToFile.cpp
+++ 44_02_ZooSaveToFile/44_02_ZooSaveToFile.cpp
@@ -4,82 +4,69 @@
 operator >> для зчитування  та запису у файл
 (Train)*/
 
-
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 using namespace std;
-
 
 struct Vagon {
     int number;
     int places;
     int passengers;
-    friend ofstream& operator <<(ofstream& out, Vagon& vagon);
-    friend ifstream& operator >>(ifstream& in, Vagon& vagon);
+
+    friend ofstream& operator<<(ofstream& out, const Vagon& vagon);
+    friend ifstream& operator>>(ifstream& in, Vagon& vagon);
 };
+
+ofstream& operator<<(ofstream& out, const Vagon& vagon)
+{
+    out << vagon.number << " " << vagon.places << " " << vagon.passengers;
+    return out;
+}
+
+ifstream& operator>>(ifstream& in, Vagon& vagon)
+{
+    in >> vagon.number >> vagon.places >> vagon.passengers;
+    return in;
+}
 
 class Train {
 private:
     string model;
-    int vagonCount;
-    Vagon* vagons;
+    vector<Vagon> vagons;
+
 public:
-    Train()
-    {
-        model = "Hyundai";
-        vagonCount = 0;
-        vagons = nullptr;
+    Train() : model("Hyundai") {}
 
-    }
-    Train(string model)
-    {
-        this->model = model;
-        vagonCount = 0;
-        vagons = nullptr;
-    }
+    Train(const string& model) : model(model) {}
+
     Train(const Train& other) {
-        this->model = other.model;
-        this->vagonCount = other.vagonCount;
+        model = other.model;
+        vagons = other.vagons;
+    }
 
-        if (vagonCount > 0) {
-            this->vagons = new Vagon[vagonCount];
-            for (int i = 0; i < vagonCount; ++i) {
-                this->vagons[i] = other.vagons[i];
-            }
-        }
-        else {
-            this->vagons = nullptr;
-        }
-    }
-    void AddVagon(Vagon vagon)
+    void AddVagon(const Vagon& vagon)
     {
-        vagonCount++;
-        Vagon* temp = new Vagon[vagonCount];
-        for (int i = 0; i < vagonCount - 1; i++)
-        {
-            temp[i] = vagons[i];
-        }
-        temp[vagonCount - 1] = vagon;
-        if (vagons != nullptr)
-            delete[] vagons;
-        vagons = temp;
+        vagons.push_back(vagon);
     }
-    void print() {
+
+    void print() const {
         cout << "Train model: " << model << endl;
-        cout << "Vagon counter: " << vagonCount << endl;
-        for (int i = 0; i < vagonCount; i++)
+        cout << "Vagon counter: " << vagons.size() << endl;
+        for (const auto& v : vagons)
         {
-            cout << "Vagon number: " << vagons[i].number << endl;
-            cout << "Vagon places: " << vagons[i].places << endl;
-            cout << "Vagon passengers: " << vagons[i].passengers << endl;
+            cout << "Vagon number: " << v.number << endl;
+            cout << "Vagon places: " << v.places << endl;
+            cout << "Vagon passengers: " << v.passengers << endl;
         }
     }
+
     void addOnePas(int vagonNumber) {
         bool found = false;
-        for (int i = 0; i < vagonCount; i++) {
-            if (vagons[i].number == vagonNumber) {
-                vagons[i].passengers++;
+        for (auto& v : vagons) {
+            if (v.number == vagonNumber) {
+                v.passengers++;
                 found = true;
                 break;
             }
@@ -89,96 +76,64 @@ public:
         }
     }
 
+    void BinaryWrite() {
+        ofstream out("Train.bin", ios::binary);
+        size_t modelSize = model.size();
+        out.write(reinterpret_cast<char*>(&modelSize), sizeof(modelSize));
+        out.write(model.c_str(), modelSize);
 
-    void BinaryWrite()
-    {
-        ofstream out("Train.bin", ios_base::out | ios_base::binary);
-        out.write((char*)&model, sizeof(model));
-        out.write((char*)&vagonCount, sizeof(vagonCount));
-        for (int i = 0; i < vagonCount; i++)
-        {
-            out.write((char*)&vagons[i], sizeof(vagons[i]));
-
+        size_t vagonCount = vagons.size();
+        out.write(reinterpret_cast<char*>(&vagonCount), sizeof(vagonCount));
+        for (const auto& v : vagons) {
+            out.write(reinterpret_cast<const char*>(&v), sizeof(Vagon));
         }
         out.close();
-        cout << "Save to file  binary" << endl;
+        cout << "Save to file (binary)" << endl;
     }
-    void BinaryRead()
-    {
-        ifstream in("Train.bin", ios_base::in | ios_base::binary);
-        in.read((char*)&model, sizeof(model));
-        in.read((char*)&vagonCount, sizeof(vagonCount));
-        vagons = new Vagon[vagonCount];
-        for (int i = 0; i < vagonCount; i++)
-        {
-            
-            in.read((char*)&vagons[i], sizeof(vagons[i]));
+
+    void BinaryRead() {
+        ifstream in("Train.bin", ios::binary);
+        if (!in) {
+            cout << "Файл не найден!" << endl;
+            return;
         }
+
+        size_t modelSize;
+        in.read(reinterpret_cast<char*>(&modelSize), sizeof(modelSize));
+        model.resize(modelSize);
+        in.read(&model[0], modelSize);
+
+        size_t vagonCount;
+        in.read(reinterpret_cast<char*>(&vagonCount), sizeof(vagonCount));
+        vagons.resize(vagonCount);
+        for (size_t i = 0; i < vagonCount; ++i) {
+            in.read(reinterpret_cast<char*>(&vagons[i]), sizeof(Vagon));
+        }
+
         in.close();
-
     }
-
-
-
-
-
-
-
-
-
-
-
-
 };
-
-ofstream& operator <<(ofstream& out, Vagon& vagon)
-{
-    out << vagon.number << " " << vagon.places << " " << vagon.passengers;
-    return out;
-}
-ifstream& operator >>(ifstream& in, Vagon& vagon)
-{
-    in >> vagon.number >> vagon.places >> vagon.passengers;
-    return in;
-}
-
 
 int main()
 {
+    Train tr;
+    Vagon vagon1{ 1, 20, 10 };
+    Vagon vagon2{ 2, 20, 10 };
+    Vagon vagon3{ 3, 20, 10 };
 
-    //Train tr;
-    //Vagon vagon1{ 1,20,10 };
-    //Vagon vagon2{ 2,20,10 };
-    //Vagon vagon3{ 3,20,10 };
-    //tr.AddVagon(vagon1);
-    //tr.AddVagon(vagon2);
-    //tr.AddVagon(vagon3);
-    //tr.print();
-    //tr.BinaryWrite();
+    tr.AddVagon(vagon1);
+    tr.AddVagon(vagon2);
+    tr.AddVagon(vagon3);
+
+    tr.print();
+    tr.BinaryWrite();
 
     Train readFromfile;
-    //readFromfile.ReadFromFile();
     readFromfile.BinaryRead();
     readFromfile.print();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    return 0;
 }
+
 
 
